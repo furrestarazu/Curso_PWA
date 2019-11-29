500a8c35f50d91ef37d83167dfc843161b8a8076
Facundo Urrestarazu <furrestarazu@outlook.com>
2019-11-28 22:49:50 -0300

    .

--- Tateti/js/app.js
+++ Tateti/js/app.js
@@ -1,7 +1,6 @@
 if("serviceWorker" in navigator)
 {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('sw.js')
     .then((reg) => console.log("Service Worker registrado",reg))
-    .catch((err) => console.log("Ocurrió un error al registrar el Service Worker",err));
+    .catch((err) => console.log("Ocurrió un error al registrar el Service Worker",err))
 }
-
