546c294a3e1156cfe83f2e9cff4dbc656fdc3c25
Facundo Urrestarazu <furrestarazu@outlook.com>
2019-12-03 21:40:56 -0300

    Activate SW

--- Tateti/sw.js
+++ Tateti/sw.js
@@ -1,9 +1,15 @@
 // install
 self.addEventListener("install",evt  =>
 {
-    console.log("Service Worker instalado correctamente");
+    console.log("Service Worker instalado");
 } );
 
+
 // activate
+self.addEventListener("activate", evt =>
+{
+    console.log("Service Worker activado");
+})
+
 
 // fetch
