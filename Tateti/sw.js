822f5dd7a7093ab830c5b6a7d6f0271860c5c4a7
Facundo Urrestarazu <furrestarazu@outlook.com>
2019-12-03 21:57:37 -0300

    fetch

--- Tateti/sw.js
+++ Tateti/sw.js
@@ -1,15 +1,19 @@
-// install
-self.addEventListener("install",evt  =>
+// install --> instala los elementos en la caché del navegador
+self.addEventListener("install", evt  =>
 {
     console.log("Service Worker instalado");
 } );
 
 
-// activate
+// activate --> verifica que no haya cambios en los archivos. En caso de haberlos, pisa con la última versión
 self.addEventListener("activate", evt =>
 {
     console.log("Service Worker activado");
-})
+});
 
 
-// fetch
+// fetch --> atrapa los pedidos al servidor
+self.addEventListener("fetch", evt =>
+{
+    console.log("se atrapó el evento: ", evt);
+});
